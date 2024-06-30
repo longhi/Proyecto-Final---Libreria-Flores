56646420dd4eea388dc084139b4184b4cb7f884f
frijolito2911 <mbaku478@gmail.com>
2024-06-30 17:27:32 -0500

    fijar barra de nevagacion y carrusel

---
+++ js/navegacion_fija.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll',function(){
+    if(this.window.scrollY > 80){
+        this.document.querySelector('.navbar').classList.add('navbar-flotante')
+    }else{
+        this.document.querySelector('.navbar').classList.remove('navbar-flotante')
+    }
+})
